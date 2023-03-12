c9728faf41ad4f058cd902f52fa30734dc968279
david-itzhak <daviditzhak7@gmail.com>
2023-03-12 10:57:53 +0200

    Initial commit

---
+++ src/components/ResetButton.js
@@ -0,0 +1,8 @@
+const ResetButton = ({onClick}) => {
+    return (
+        <div>
+            <button style={{backgroundColor: "lightgreen"}} onClick={onClick}>Reset</button>
+        </div>
+    );
+}
+export default ResetButton;
